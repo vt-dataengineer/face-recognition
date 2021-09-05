a6ff1d4171e472e22eebd302173ccc6cb93da69c
vt-dataengineer <33867147+vt-dataengineer@users.noreply.github.com>
2021-09-05 11:28:02 +0530

    Update face-recognition.py

--- face-recognition.py
+++ face-recognition.py
@@ -14,16 +14,13 @@ webcam = cv2.VideoCapture(0)
 while True:
     # read the current frame
     successful_frame_read, frame = webcam.read()
-
     # convert to grayscale
     grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # detect faces
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
     # draw rectanglearound faces
     for (x,y,w,h) in face_coordinates:
       cv2.rectangle(frame, (x,y), (x+w,y+h), (randrange(256),randrange(256),randrange(256)), 2)
-
 
     cv2.imshow('Face recognition', frame)
     key = cv2.waitKey(1)
